b9628f519710c18a60b67d10ed5eb74092bf7b8b
Phairat Lin <yangsheng104@gmail.com>
2019-11-05 00:04:49 +0700

    Completed Q2

--- a3/a3-handout/det/det.c
+++ a3/a3-handout/det/det.c
@@ -2,10 +2,44 @@
 #include <stdlib.h>
 #include <string.h>
 
+int** makeMatrix(int** a, int** matrix, int i, int new_i, int n);
+
 double compute_det(int **a, int n) {
-  // implement this
-  return 0.0;
+  int changeSign = 1;
+  double sum = 0;
+
+  if (n==1){
+    return a[0][0];
+  }else{
+    int** new_matrix = (int**)malloc(sizeof(int *) * n-1);
+    for (int i=0; i < n; i++){
+      new_matrix[i] = (int *)malloc(sizeof(int) * n-1);
+    }
+    for (int column=0; column < n; column++){
+      int new_col = 0;
+      new_matrix = makeMatrix(a, new_matrix, column, new_col, n); 
+      
+    sum+=(changeSign * a[column][0] * compute_det(new_matrix, n-1));
+    changeSign*=(-1);
+    }
+  }
+  return sum;
 }
+
+int** makeMatrix(int** a, int** matrix, int column, int new_col, int n){
+  for (int row=0; row < n; row++){
+    if (column != row){
+      for (int new_row=1; new_row < n; new_row++){
+        matrix[new_col][new_row-1] = a[row][new_row];
+      }
+      new_col++;
+    }
+  }
+
+  return matrix;
+}
+
+
 
 /*
 TEST: ./det < det.in
@@ -13,6 +47,23 @@ OUTPUT:
 -105.00000
 */
 int main(void) {
-  
-  // implement this
+  int n;
+
+  scanf("%d", &n);
+
+  int** matrix = (int **)malloc(sizeof(int*) * n);
+  for (int i=0; i < n; i++){
+      matrix[i] = (int *)malloc(sizeof(int) * n);
+  }
+
+  for (int i=0; i < n; i++){
+    for (int j=0; j < n; j++){
+      scanf("%d", &matrix[i][j]);
+    }
+  }
+
+  printf("%f \n",compute_det(matrix, n));
+
+
+
 }
